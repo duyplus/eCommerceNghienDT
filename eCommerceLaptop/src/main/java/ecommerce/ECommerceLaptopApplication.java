3bed050f54017a24286b11e23f8306009e18e1ef
DUYPLUS <duyplusdz@gmail.com>
2022-09-24 22:12:20 +0700

    first commit

---
+++ eCommerceLaptop/src/main/java/ecommerce/ECommerceLaptopApplication.java
@@ -0,0 +1,13 @@
+package ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceLaptopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceLaptopApplication.class, args);
+	}
+
+}
