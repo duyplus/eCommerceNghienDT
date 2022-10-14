c6324ce39b323bdf5da759d1149095deec92ae39
DUYPLUS <duyplusdz@gmail.com>
2022-10-15 00:34:26 +0700

    upload

---
+++ Frontend/admin/assets/js/app.js
@@ -0,0 +1,162 @@
+(function ($) {
+    'use strict';
+
+    $('#summernote').summernote({
+        height: 200,
+        placeholder: 'Nhập thông tin sản phẩm..',
+        toolbar: [
+            ['style', ['bold', 'italic', 'underline', 'clear']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['view', ['fullscreen', 'codeview']],
+        ]
+    });
+
+    $('#datatable').DataTable();
+    var table = $('#datatable-buttons').DataTable({
+        responsive: true,
+        lengthChange: false,
+        buttons: ['excel', 'pdf', 'csv', 'print']
+    });
+    table.buttons().container().appendTo('#datatable-buttons_wrapper .col-md-6:eq(0)');
+
+    $('#row_callback').DataTable({
+        "createdRow": function (row, data, index) {
+            if (data[5].replace(/[\$,]/g, '') * 1 > 150000) {
+                $('td', row).eq(5).addClass('highlight');
+            }
+        }
+    });
+
+    function initMetisMenu() {
+        //metis menu
+        $(".metismenu").metisMenu();
+        $(window).resize(function () {
+            initEnlarge();
+        });
+    }
+
+    function initLeftMenuCollapse() {
+        // Left menu collapse
+        $('.button-menu-mobile').on('click', function (event) {
+            event.preventDefault();
+            $("body").toggleClass("enlarge-menu");
+            $("body").toggleStyle("enlarge-menu");
+        });
+    }
+
+    function initEnlarge() {
+        if ($(window).width() < 1025) {
+            $('body').addClass('enlarge-menu');
+        } else {
+            // if ($('body').data('keep-enlarged') != true)
+            $('body').removeClass('enlarge-menu');
+        }
+    }
+
+    function initTooltipPlugin() {
+        $('[data-toggle="tooltip"]').tooltip()
+    }
+
+    function initMainIconTabMenu() {
+        $('.main-icon-menu .nav-link').on('click', function (e) {
+            $("body").removeClass("enlarge-menu");
+            e.preventDefault();
+            $(this).addClass('active');
+            $(this).siblings().removeClass('active');
+            $('.main-menu-inner').addClass('active');
+            var targ = $(this).attr('href');
+            $(targ).addClass('active');
+            $(targ).siblings().removeClass('active');
+        });
+    }
+
+    function initActiveMenu() {
+        // === following js will activate the menu in left side bar based on url ====
+        $(".leftbar-tab-menu a, .left-sidenav a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).addClass("active");
+                $(this).parent().addClass("active"); // add active to li of the current link                 
+                $(this).parent().parent().addClass("in");
+                $(this).parent().parent().addClass("mm-show");
+                $(this).parent().parent().parent().addClass("mm-active");
+                $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().parent().addClass("active");
+                $(this).parent().parent().parent().parent().addClass("mm-show"); // add active to li of the current link                
+                $(this).parent().parent().parent().parent().parent().addClass("mm-active");
+                var menu = $(this).closest('.main-icon-menu-pane').attr('id');
+                $("a[href='#" + menu + "']").addClass('active');
+
+            }
+        });
+    }
+
+    function initFeatherIcon() {
+        feather.replace()
+    }
+
+    function initMainIconMenu() {
+        $(".navigation-menu a").each(function () {
+            var pageUrl = window.location.href.split(/[?#]/)[0];
+            if (this.href == pageUrl) {
+                $(this).parent().addClass("active"); // add active to li of the current link
+                $(this).parent().parent().parent().addClass("active"); // add active class to an anchor
+                $(this).parent().parent().parent().parent().parent().addClass("active"); // add active class to an anchor
+            }
+        });
+    }
+
+    function initTopbarMenu() {
+        $('.navbar-toggle').on('click', function (event) {
+            $(this).toggleClass('open');
+            $('#navigation').slideToggle(400);
+        });
+        $('.navigation-menu>li').slice(-2).addClass('last-elements');
+        $('.navigation-menu li.has-submenu a[href="#"]').on('click', function (e) {
+            if ($(window).width() < 992) {
+                e.preventDefault();
+                $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
+            }
+        });
+    }
+
+    function initDropify() {
+        $('.dropify').dropify();
+        var drEvent = $('#input-file-events').dropify();
+        drEvent.on('dropify.beforeClear', function (event, element) {
+            return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
+        });
+        drEvent.on('dropify.afterClear', function (event, element) {
+            alert('File deleted');
+        });
+        drEvent.on('dropify.errors', function (event, element) {
+            console.log('Has Errors');
+        });
+        var drDestroy = $('#input-file-to-destroy').dropify();
+        drDestroy = drDestroy.data('dropify')
+        $('#toggleDropify').on('click', function (e) {
+            e.preventDefault();
+            if (drDestroy.isDropified()) {
+                drDestroy.destroy();
+            } else {
+                drDestroy.init();
+            }
+        })
+    }
+
+    function init() {
+        initMetisMenu();
+        initLeftMenuCollapse();
+        initEnlarge();
+        initTooltipPlugin();
+        initMainIconTabMenu();
+        initActiveMenu();
+        initFeatherIcon();
+        initMainIconMenu();
+        initTopbarMenu();
+        initDropify();
+        Waves.init();
+    }
+    init();
+
+})(jQuery)
