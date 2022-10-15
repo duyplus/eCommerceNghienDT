357c7ffac2501b1929a0ddaba0312332f23a7f09
DUYPLUS <duyplusdz@gmail.com>
2022-10-15 23:49:44 +0700

    update service user

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,27 @@
 package service;
 
-public interface UserService {
+import entity.User;
 
+import java.util.List;
+
+public interface UserService {
+    User findbyId(String username);
+
+    List<User> findAll();
+
+    User create(User user);
+
+    User update(User user);
+
+    void delete(String username);
+
+//    void loginFromOAuth2(OAuth2AuthenticationToken oauth2);
+//
+//    void updateToken(String token, String email) throws Exception;
+//
+//    User getByToken(String token);
+//
+//    void updatePassword(User entity, String newPassword);
+//
+//    void changePassword(User entity, String newPassword);
 }
