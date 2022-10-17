5de4cc8ab4d083eca447d95123e58941fda8eec0
DUYPLUS <duyplusdz@gmail.com>
2022-10-18 02:51:40 +0700

    Update

--- src/main/java/nghiendt/common/DateUtils.java
+++ src/main/java/nghiendt/common/DateUtils.java
@@ -1,9 +1,31 @@
 package nghiendt.common;
 
+import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtils {
+
+    private final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    private final SimpleDateFormat DATE_TIME_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+    private Date parseDate(String date) {
+        try {
+            return new Date(DATE_FORMAT.parse(date).getTime());
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private Timestamp parseTimestamp(String timestamp) {
+        try {
+            return new Timestamp(DATE_TIME_FORMAT.parse(timestamp).getTime());
+        } catch (ParseException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
 
     public static String formatDate(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
