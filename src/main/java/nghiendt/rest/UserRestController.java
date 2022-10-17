7cf931c46c049296fafe0a27e5120c2ad39e89cb
DUYPLUS <duyplusdz@gmail.com>
2022-10-18 02:52:34 +0700

    Update user rest

--- src/main/java/nghiendt/rest/UserRestController.java
+++ src/main/java/nghiendt/rest/UserRestController.java
@@ -1,50 +1,60 @@
 package nghiendt.rest;
 
 import nghiendt.entity.User;
+import nghiendt.exception.ResourceNotFoundException;
+import nghiendt.repository.UserRepository;
 import nghiendt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/api/user")
 public class UserRestController {
     @Autowired
-    UserService userService;
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
-    public List<User> getAllUsers() {
-        return userService.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> listUsers = userRepository.findAll();
+        if (listUsers.isEmpty()) {
+            return new ResponseEntity<List<User>>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<List<User>>(listUsers, HttpStatus.OK);
     }
 
-//    @GetMapping
-//    public ResponseEntity<List<User>> getAllUsers() {
-//        List<User> listUser = userService.findAll();
-//        if (listUser.isEmpty()) {
-//            return new ResponseEntity(HttpStatus.NO_CONTENT);
-//        }
-//        return new ResponseEntity<List<User>>(listUser, HttpStatus.OK);
-//    }
-
     @GetMapping("{id}")
-    public User getOne(@PathVariable("id") String id) {
-        return userService.findbyId(id);
+    public ResponseEntity<User> getUserById(@PathVariable String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + id));
+        return ResponseEntity.ok(user);
     }
 
     @PostMapping
     public User create(@RequestBody User user) {
-        return userService.create(user);
+        return userRepository.save(user);
     }
 
-    @PutMapping
-    public User update(@RequestBody User user) {
-        return userService.update(user);
+    @PutMapping("{id}")
+    public ResponseEntity<User> update(@PathVariable("id") String id, @RequestBody User user) {
+        User updateUser = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id:" + id));
+        userRepository.save(user);
+        return ResponseEntity.ok(updateUser);
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable("id") String id) {
-        userService.delete(id);
+    public ResponseEntity<HttpStatus> delete(@PathVariable String id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not exist with id: " + id));
+        userRepository.delete(user);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
