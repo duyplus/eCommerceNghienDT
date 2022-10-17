0452aadb93c451a98c506b5b760755a50c72a27d
DUYPLUS <duyplusdz@gmail.com>
2022-10-18 02:53:10 +0700

    Update

--- src/main/java/nghiendt/ECommerceNghienDTApplication.java
+++ src/main/java/nghiendt/ECommerceNghienDTApplication.java
@@ -2,7 +2,9 @@ package nghiendt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class ECommerceNghienDTApplication {
     public static void main(String[] args) {
