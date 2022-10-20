f884dc84e08bbf317b624cf8058a092ca0e59a45
DUYPLUS <duyplusdz@gmail.com>
2022-10-21 00:40:17 +0700

    Update frontend

--- Frontend/admin/assets/ctrl/app.js
+++ Frontend/admin/assets/ctrl/app.js
@@ -2,33 +2,33 @@ const app = angular.module("myApp", ['ngRoute']);
 app.config(function ($routeProvider, $locationProvider) {
     // route
     $routeProvider
-        .when("/category-list", { templateUrl: "pages/category-list.html", controller: "category-ctrl" })
-        .when("/category-form", { templateUrl: "pages/category-form.html", controller: "category-ctrl" })
+        .when("/category-list", { templateUrl: "admin/pages/category-list.html", controller: "category-ctrl" })
+        .when("/category-form", { templateUrl: "admin/pages/category-form.html", controller: "category-ctrl" })
 
-        .when("/company-list", { templateUrl: "pages/company-list.html", controller: "company-ctrl" })
-        .when("/company-form", { templateUrl: "pages/company-form.html", controller: "company-ctrl" })
+        .when("/company-list", { templateUrl: "admin/pages/company-list.html", controller: "company-ctrl" })
+        .when("/company-form", { templateUrl: "admin/pages/company-form.html", controller: "company-ctrl" })
 
-        .when("/product-list", { templateUrl: "pages/product-list.html", controller: "product-ctrl" })
-        .when("/product-form", { templateUrl: "pages/product-form.html", controller: "product-ctrl" })
+        .when("/product-list", { templateUrl: "admin/pages/product-list.html", controller: "product-ctrl" })
+        .when("/product-form", { templateUrl: "admin/pages/product-form.html", controller: "product-ctrl" })
 
-        .when("/order-approval", { templateUrl: "pages/order-approval.html", controller: "order-ctrl" })
-        .when("/order-cancel", { templateUrl: "pages/order-cancel.html", controller: "order-ctrl" })
-        .when("/order-form", { templateUrl: "pages/order-form.html", controller: "order-ctrl" })
-        .when("/order-list", { templateUrl: "pages/order-list.html", controller: "order-ctrl" })
-        .when("/orderdetail", { templateUrl: "pages/orderdetail.html", controller: "order-ctrl" })
+        .when("/order-approval", { templateUrl: "admin/pages/order-approval.html", controller: "order-ctrl" })
+        .when("/order-cancel", { templateUrl: "admin/pages/order-cancel.html", controller: "order-ctrl" })
+        .when("/order-form", { templateUrl: "admin/pages/order-form.html", controller: "order-ctrl" })
+        .when("/order-list", { templateUrl: "admin/pages/order-list.html", controller: "order-ctrl" })
+        .when("/orderdetail", { templateUrl: "admin/pages/orderdetail.html", controller: "order-ctrl" })
 
-        .when("/review-list", { templateUrl: "pages/review-list.html", controller: "review-ctrl" })
-        .when("/review-form", { templateUrl: "pages/review-form.html", controller: "review-ctrl" })
+        .when("/review-list", { templateUrl: "admin/pages/review-list.html", controller: "review-ctrl" })
+        .when("/review-form", { templateUrl: "admin/pages/review-form.html", controller: "review-ctrl" })
 
-        .when("/user-list", { templateUrl: "/pages/user-list.html", controller: "user-ctrl" })
-        .when("/user-form", { templateUrl: "/pages/user-form.html", controller: "user-ctrl" })
+        .when("/user-list", { templateUrl: "admin/pages/user-list.html", controller: "user-ctrl" })
+        .when("/user-form", { templateUrl: "admin/pages/user-form.html", controller: "user-ctrl" })
 
-        .when("/statistic", { templateUrl: "/pages/statistic.html", controller: "statistic-ctrl" })
-        .when("/setting", { templateUrl: "/pages/setting.html", controller: "setting-ctrl" })
+        .when("/statistic", { templateUrl: "admin/pages/statistic.html", controller: "statistic-ctrl" })
+        .when("/setting", { templateUrl: "admin/pages/setting.html", controller: "setting-ctrl" })
 
-        .when("/login", { templateUrl: "/pages/login.html" })
-        .when("/404", { templateUrl: "/pages/404.html" })
-        .when('/', { templateUrl: 'pages/home.html', })
+        .when("/login", { templateUrl: "admin/pages/login.html" })
+        .when("/404", { templateUrl: "admin/pages/404.html" })
+        .when('/', { templateUrl: 'admin/pages/home.html', })
         .otherwise({ redirectTo: '/' });
 });
 
