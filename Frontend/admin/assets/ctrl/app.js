9604bc956aa7080042487da0bbcb8bf7eac7040f
DUYPLUS <duyplusdz@gmail.com>
2022-10-22 05:32:29 +0700

    CRUD User

--- Frontend/admin/assets/ctrl/app.js
+++ Frontend/admin/assets/ctrl/app.js
@@ -1,4 +1,4 @@
-const app = angular.module("myApp", ['ngRoute']);
+const app = angular.module("myApp", ['ngRoute', 'ui.bootstrap']);
 app.config(function ($routeProvider, $locationProvider) {
     // route
     $routeProvider
@@ -31,46 +31,3 @@ app.config(function ($routeProvider, $locationProvider) {
         .when('/', { templateUrl: 'admin/pages/home.html', })
         .otherwise({ redirectTo: '/' });
 });
-
-var dataTable = {
-    vi: {
-        "sProcessing": "Đang xử lý...",
-        "sLengthMenu": "Xem _MENU_ mục",
-        "sZeroRecords": "Không tìm thấy dòng nào phù hợp",
-        "sInfo": "Đang xem <b>_START_</b> đến <b>_END_</b> trong tổng số <b>_TOTAL_</b> mục",
-        "sInfoEmpty": "Đang xem <b>0</b> đến <b>0</b> trong tổng số <b>0</b> mục",
-        "sInfoFiltered": "(được lọc trong <b>_MAX_</b> mục)",
-        "sInfoPostFix": "",
-        "sSearch": "Tìm kiếm:",
-        "searchPlaceholder": "Nhập tìm kiếm...",
-        "sUrl": "",
-        "oPaginate": {
-            "sFirst": "Đầu",
-            "sPrevious": "Trước",
-            "sNext": "Sau",
-            "sLast": "Cuối"
-        }
-    },
-    en: {
-        records: {
-            processing: 'Please wait...',
-            noRecords: 'No records found'
-        },
-        toolbar: {
-            pagination: {
-                items: {
-                    default: {
-                        first: 'First',
-                        prev: 'Previous',
-                        next: 'Next',
-                        last: 'Last',
-                        more: 'More pages',
-                        input: 'Page number',
-                        select: 'Select page size'
-                    },
-                    info: 'Displaying {{start}} - {{end}} of {{total}} records'
-                }
-            }
-        }
-    }
-};
--- Frontend/admin/assets/ctrl/user-ctrl.js
+++ Frontend/admin/assets/ctrl/user-ctrl.js
@@ -1,4 +1,4 @@
-app.controller("user-ctrl", function ($scope, $http, $compile) {
+app.controller("user-ctrl", function ($scope, $rootScope, $http, $compile) {
     var url = "http://localhost:8080/api/user";
     $scope.items = [];
     $scope.form = {};
@@ -23,47 +23,19 @@ app.controller("user-ctrl", function ($scope, $http, $compile) {
     $scope.initialize = function () {
         //load data
         $http.get(url).then(resp => {
-            var table = $('#datatable').DataTable({
-                data: resp.data,
-                dom: `<'row'<'col-sm-2 text-left'f><'col-sm-10 text-right'B>>
-                      <'row'<'col-sm-12'tr>>
-                      <'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7 dataTables_pager'lp>>`,
-                columns: [
-                    { data: 'username' },
-                    { data: 'password' },
-                    { data: 'fullname' },
-                    { data: 'phone' },
-                    { data: 'birthday', render: DataTable.render.datetime('DD/MM/yyyy') },
-                    { data: 'email' },
-                    { data: 'address' },
-                    { data: 'createdat', render: DataTable.render.datetime('HH:mm - DD/MM/yyyy') },
-                    { data: 'updatedat', render: DataTable.render.datetime('HH:mm - DD/MM/yyyy') },
-                    { data: null },
-                ],
-                columnDefs: [
-                    {
-                        targets: -1,
-                        data: null,
-                        defaultContent: '<a href="#!user-form" ng-click="edit(item)"><i class="las la-pen text-info"></i></a>',
-                    }
-                ],
-                responsive: true,
-                lengthChange: false,
-                language: dataTable.vi,
-                buttons: ['excel', 'pdf', 'csv', 'print']
+            $scope.items = resp.data;
 
-            });
-            table.buttons().container().appendTo('#datatable_wrapper .col-md-6:eq(0)');
-            $('#row_callback').DataTable({
-                "createdRow": function (row, data, index) {
-                    if (data[5].replace(/[\$,]/g, '') * 1 > 150000) {
-                        $('td', row).eq(5).addClass('highlight');
-                    }
-                    if (!row.compiled) {
-                        $compile(angular.element(row))($scope);
-                        row.compiled = true;
-                    }
-                }
+            $scope.curPage = 1;
+            $scope.itemsPerPage = 10;
+            $scope.maxSize = 5;
+            this.items = $scope.items;
+            $scope.numOfPages = function () {
+                return Math.ceil($scope.items.length / $scope.itemsPerPage);
+            };
+            $scope.$watch('curPage + numPerPage', function () {
+                var begin = (($scope.curPage - 1) * $scope.itemsPerPage);
+                var end = begin + $scope.itemsPerPage;
+                $scope.filteredItems = $scope.items.slice(begin, end);
             });
         });
 
@@ -97,24 +69,26 @@ app.controller("user-ctrl", function ($scope, $http, $compile) {
 
     //xoa form
     $scope.reset = function () {
-        $scope.form = {
-            username: '',
-        };
+        $scope.form = {};
     }
 
     //hien thi len form
     $scope.edit = function (item) {
         $scope.form = angular.copy(item);
+        $scope.form.birthday = new Date($scope.form.birthday);
+        $("#edit").modal('show');
     }
 
     //them sp moi
     $scope.create = function () {
         var item = angular.copy($scope.form);
         $http.post(`${url}`, item).then(resp => {
+            resp.data.birthday = new Date(resp.data.birthday);
             resp.data.token = "null";
             $scope.items.push(resp.data);
             $scope.reset();
             sweetalert("Thêm mới thành công!");
+            $("#edit").modal('hide');
         }).catch(error => {
             sweetalert("Lỗi thêm mới tài khoản!");
             console.log("Error", error);
@@ -124,11 +98,12 @@ app.controller("user-ctrl", function ($scope, $http, $compile) {
     //cap nhat sp
     $scope.update = function () {
         var item = angular.copy($scope.form);
-        $http.put(`${url}/${item.id}`, item).then(resp => {
-            var index = $scope.items.findIndex(p => p.id == item.id);
+        $http.put(`${url}/${item.username}`, item).then(resp => {
+            var index = $scope.items.findIndex(p => p.username == item.username);
             $scope.items[index] = item;
             $scope.reset();
             sweetalert("Cập nhật tài khoản thành công!");
+            $("#edit").modal('hide');
         }).catch(error => {
             sweetalert("Lỗi cập nhật tài khoản!");
             console.log("Error", error);
@@ -137,14 +112,61 @@ app.controller("user-ctrl", function ($scope, $http, $compile) {
 
     //xoa sp
     $scope.delete = function (item) {
-        $http.delete(`${url}/${item.id}`).then(resp => {
-            var index = $scope.items.findIndex(p => p.id == item.id);
+        $http.delete(`${url}/${item.username}`).then(resp => {
+            var index = $scope.items.findIndex(p => p.username == item.username);
             $scope.items.splice(index, 1);
             $scope.reset();
             sweetalert("Xóa tài khoản thành công!");
+            $("#edit").modal('hide');
         }).catch(error => {
             sweetalert("Lỗi xóa tài khoản!");
             console.log("Error", error);
         });
+    }
+
+    //phan trang
+    $scope.pager = {
+        page: 0,
+        size: 10,
+        get items() {
+            var start = this.page * this.size;
+            return $scope.items.slice(start, start + this.size);
+        },
+        get count() {
+            return Math.ceil(1.0 * $scope.items.length / this.size)
+        },
+        first() {
+            this.page = 0;
+        },
+        prev() {
+            this.page--;
+            if (this.page < 0) {
+                this.last();
+            }
+        },
+        next() {
+            this.page++;
+            if (this.page >= this.count) {
+                this.first();
+            }
+        },
+        last() {
+            this.page = this.count - 1;
+        }
+    }
+});
+app.directive('firebaseDate', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, ngModel) {
+            ngModel.$formatters.push(function (fromModel) {
+                fromModel = new Date(fromModel);
+                return fromModel;
+            });
+            ngModel.$parsers.push(function (fromField) {
+                fromField = fromField.getTime();
+                return fromField;
+            });
+        }
     }
 });
