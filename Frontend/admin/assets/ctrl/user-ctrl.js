a35647b86eac4b4e7673694d0602756e610eb808
nghitruong <89492293+vntruongdev@users.noreply.github.com>
2022-10-22 18:04:10 +0700

    form user edit

--- Frontend/admin/assets/ctrl/user-ctrl.js
+++ Frontend/admin/assets/ctrl/user-ctrl.js
@@ -1,172 +1,203 @@
-app.controller("user-ctrl", function ($scope, $rootScope, $http, $compile) {
+app
+  .run(($rootScope) => {
+    $rootScope.items = [];
+    $rootScope.form = {};
+  })
+  .controller("user-ctrl", function ($scope, $rootScope, $http, $compile) {
     var url = "http://localhost:8080/api/user";
-    $scope.items = [];
-    $scope.form = {};
-
+    // $scope.form = $rootScope.form;
     var sweetalert = function (text) {
-        Swal.fire({
-            icon: "success",
-            title: text,
-            showConfirmButton: false,
-            timer: 2000,
-        });
-    }
+      Swal.fire({
+        icon: "success",
+        title: text,
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    };
     var sweetalert_error = function (text) {
-        Swal.fire({
-            icon: "error",
-            title: text,
-            showConfirmButton: false,
-            timer: 2000,
-        });
-    }
+      Swal.fire({
+        icon: "error",
+        title: text,
+        showConfirmButton: false,
+        timer: 2000,
+      });
+    };
 
     $scope.initialize = function () {
-        //load data
-        $http.get(url).then(resp => {
-            $scope.items = resp.data;
+      //load data
+      $http.get(url).then((resp) => {
+        $scope.items = resp.data;
+      });
+      $scope.items = [
+        {
+          username: "admin",
+          phone: "111",
+          password: "111",
+          fullname: "Nghi",
+          birthday: "",
+          email: "",
+          address: "",
+          image: "",
+          created_at: "",
+          updated_at: "",
+          token: "",
+        },
+      ];
 
-            $scope.curPage = 1;
-            $scope.itemsPerPage = 10;
-            $scope.maxSize = 5;
-            this.items = $scope.items;
-            $scope.numOfPages = function () {
-                return Math.ceil($scope.items.length / $scope.itemsPerPage);
-            };
-            $scope.$watch('curPage + numPerPage', function () {
-                var begin = (($scope.curPage - 1) * $scope.itemsPerPage);
-                var end = begin + $scope.itemsPerPage;
-                $scope.filteredItems = $scope.items.slice(begin, end);
-            });
-        });
+      $scope.curPage = 1;
+      $scope.itemsPerPage = 10;
+      $scope.maxSize = 5;
+      this.items = $scope.items;
+      $scope.numOfPages = function () {
+        return Math.ceil($scope.items.length / $scope.itemsPerPage);
+      };
+      $scope.$watch("curPage + numPerPage", function () {
+        var begin = ($scope.curPage - 1) * $scope.itemsPerPage;
+        var end = begin + $scope.itemsPerPage;
+        $scope.filteredItems = $scope.items.slice(begin, end);
+      });
+      // Summernote
+      $("#summernote").summernote({
+        height: 200,
+        placeholder: "Nhập thông tin sản phẩm..",
+        toolbar: [
+          ["style", ["bold", "italic", "underline", "clear"]],
+          ["para", ["ul", "ol", "paragraph"]],
+          ["view", ["codeview"]],
+        ],
+      });
 
-        // Summernote
-        $('#summernote').summernote({
-            height: 200,
-            placeholder: 'Nhập thông tin sản phẩm..',
-            toolbar: [
-                ['style', ['bold', 'italic', 'underline', 'clear']],
-                ['para', ['ul', 'ol', 'paragraph']],
-                ['view', ['codeview']],
-            ]
-        });
-
-        // Dropify
-        $('.dropify').dropify();
-        var drEvent = $('.dropify-event').dropify();
-        drEvent.on('dropify.beforeClear', function (event, element) {
-            return confirm("Do you really want to delete \"" + element.file.name + "\" ?");
-        });
-        drEvent.on('dropify.afterClear', function (event, element) {
-            sweetalert("File deleted!");
-        });
-        drEvent.on('dropify.errors', function (event, element) {
-            sweetalert_error("Has Errors!");
-        });
-    }
+      // Dropify
+      $(".dropify").dropify();
+      var drEvent = $(".dropify-event").dropify();
+      drEvent.on("dropify.beforeClear", function (event, element) {
+        return confirm(
+          'Do you really want to delete "' + element.file.name + '" ?'
+        );
+      });
+      drEvent.on("dropify.afterClear", function (event, element) {
+        sweetalert("File deleted!");
+      });
+      drEvent.on("dropify.errors", function (event, element) {
+        sweetalert_error("Has Errors!");
+      });
+    };
 
     //khoi dau
     $scope.initialize();
 
     //xoa form
     $scope.reset = function () {
-        $scope.form = {};
-    }
+      $scope.form = {};
+    };
 
     //hien thi len form
     $scope.edit = function (item) {
-        $scope.form = angular.copy(item);
-        $scope.form.birthday = new Date($scope.form.birthday);
-        $("#edit").modal('show');
-    }
+      $rootScope.form = angular.copy(item);
+      //   $("#edit").modal("show");
+    };
 
     //them sp moi
     $scope.create = function () {
-        var item = angular.copy($scope.form);
-        $http.post(`${url}`, item).then(resp => {
-            resp.data.birthday = new Date(resp.data.birthday);
-            resp.data.token = "null";
-            $scope.items.push(resp.data);
-            $scope.reset();
-            sweetalert("Thêm mới thành công!");
-            $("#edit").modal('hide');
-        }).catch(error => {
-            sweetalert("Lỗi thêm mới tài khoản!");
-            console.log("Error", error);
+      var item = angular.copy($scope.form);
+      $http
+        .post(`${url}`, item)
+        .then((resp) => {
+          resp.data.birthday = new Date(resp.data.birthday);
+          resp.data.token = "null";
+          $scope.items.push(resp.data);
+          $scope.reset();
+          sweetalert("Thêm mới thành công!");
+          $("#edit").modal("hide");
+        })
+        .catch((error) => {
+          sweetalert("Lỗi thêm mới tài khoản!");
+          console.log("Error", error);
         });
-    }
+    };
 
     //cap nhat sp
     $scope.update = function () {
-        var item = angular.copy($scope.form);
-        $http.put(`${url}/${item.username}`, item).then(resp => {
-            var index = $scope.items.findIndex(p => p.username == item.username);
-            $scope.items[index] = item;
-            $scope.reset();
-            sweetalert("Cập nhật tài khoản thành công!");
-            $("#edit").modal('hide');
-        }).catch(error => {
-            sweetalert("Lỗi cập nhật tài khoản!");
-            console.log("Error", error);
+      var item = angular.copy($scope.form);
+      $http
+        .put(`${url}/${item.username}`, item)
+        .then((resp) => {
+          var index = $scope.items.findIndex(
+            (p) => p.username == item.username
+          );
+          $scope.items[index] = item;
+          $scope.reset();
+          sweetalert("Cập nhật tài khoản thành công!");
+          $("#edit").modal("hide");
+        })
+        .catch((error) => {
+          sweetalert("Lỗi cập nhật tài khoản!");
+          console.log("Error", error);
         });
-    }
+    };
 
     //xoa sp
     $scope.delete = function (item) {
-        $http.delete(`${url}/${item.username}`).then(resp => {
-            var index = $scope.items.findIndex(p => p.username == item.username);
-            $scope.items.splice(index, 1);
-            $scope.reset();
-            sweetalert("Xóa tài khoản thành công!");
-            $("#edit").modal('hide');
-        }).catch(error => {
-            sweetalert("Lỗi xóa tài khoản!");
-            console.log("Error", error);
+      $http
+        .delete(`${url}/${item.username}`)
+        .then((resp) => {
+          var index = $scope.items.findIndex(
+            (p) => p.username == item.username
+          );
+          $scope.items.splice(index, 1);
+          $scope.reset();
+          sweetalert("Xóa tài khoản thành công!");
+          $("#edit").modal("hide");
+        })
+        .catch((error) => {
+          sweetalert("Lỗi xóa tài khoản!");
+          console.log("Error", error);
         });
-    }
+    };
 
     //phan trang
     $scope.pager = {
-        page: 0,
-        size: 10,
-        get items() {
-            var start = this.page * this.size;
-            return $scope.items.slice(start, start + this.size);
-        },
-        get count() {
-            return Math.ceil(1.0 * $scope.items.length / this.size)
-        },
-        first() {
-            this.page = 0;
-        },
-        prev() {
-            this.page--;
-            if (this.page < 0) {
-                this.last();
-            }
-        },
-        next() {
-            this.page++;
-            if (this.page >= this.count) {
-                this.first();
-            }
-        },
-        last() {
-            this.page = this.count - 1;
+      page: 0,
+      size: 10,
+      get items() {
+        var start = this.page * this.size;
+        return $scope.items.slice(start, start + this.size);
+      },
+      get count() {
+        return Math.ceil((1.0 * $scope.items.length) / this.size);
+      },
+      first() {
+        this.page = 0;
+      },
+      prev() {
+        this.page--;
+        if (this.page < 0) {
+          this.last();
         }
-    }
-});
-app.directive('firebaseDate', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, element, attrs, ngModel) {
-            ngModel.$formatters.push(function (fromModel) {
-                fromModel = new Date(fromModel);
-                return fromModel;
-            });
-            ngModel.$parsers.push(function (fromField) {
-                fromField = fromField.getTime();
-                return fromField;
-            });
+      },
+      next() {
+        this.page++;
+        if (this.page >= this.count) {
+          this.first();
         }
-    }
+      },
+      last() {
+        this.page = this.count - 1;
+      },
+    };
+  });
+app.directive("firebaseDate", function () {
+  return {
+    require: "ngModel",
+    link: function (scope, element, attrs, ngModel) {
+      ngModel.$formatters.push(function (fromModel) {
+        fromModel = new Date(fromModel);
+        return fromModel;
+      });
+      ngModel.$parsers.push(function (fromField) {
+        fromField = fromField.getTime();
+        return fromField;
+      });
+    },
+  };
 });
