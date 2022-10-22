6bbf3b81e6242ef0d297cb46bbf883389166edc8
DUYPLUS <duyplusdz@gmail.com>
2022-10-23 01:21:50 +0700

    Update

--- src/main/java/nghiendt/dto/OrderDetailDTO.java
+++ src/main/java/nghiendt/dto/OrderDetailDTO.java
@@ -1,24 +1,7 @@
 package nghiendt.dto;
 
-import lombok.Data;
-import nghiendt.entity.Order;
-import nghiendt.entity.Product;
-
-import javax.validation.constraints.NotNull;
-import java.io.Serializable;
-
 /**
  * A DTO for the {@link nghiendt.entity.OrderDetail} entity
  */
-@Data
-public class OrderDetailDTO implements Serializable {
-    private final Integer id;
-    @NotNull
-    private final Double price;
-    @NotNull
-    private final Integer quantity;
-    @NotNull
-    private final Order order;
-    @NotNull
-    private final Product product;
+public class OrderDetailDTO {
 }
