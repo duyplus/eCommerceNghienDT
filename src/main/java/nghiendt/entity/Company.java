f58c7c8b194a07cf2139123894ac731b6b8da27b
duyndps18393 <duyndps18393@fpt.edu.vn>
2022-10-26 14:07:27 +0700

    Đã fix

--- src/main/java/nghiendt/entity/Company.java
+++ src/main/java/nghiendt/entity/Company.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Nationalized;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -19,6 +16,7 @@ import javax.validation.constraints.Size;
 @Table(name = "Companies")
 public class Company {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Integer id;
 
