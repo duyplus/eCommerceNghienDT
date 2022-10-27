1197042bc0849ef140e757df7ef78dcfa64f37e0
duyndps18393 <duyndps18393@fpt.edu.vn>
2022-10-28 03:19:46 +0700

    Test Spring Rest Data ---- m thiếu @Repository bên Product nha Duy :)

--- src/main/java/nghiendt/repository/ProductRepository.java
+++ src/main/java/nghiendt/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package nghiendt.repository;
 
 import nghiendt.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
