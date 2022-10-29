0c8196e6b57c703b07be9341cdb6307258aa834e
duyplus <duyplusdz@gmail.com>
2022-10-30 00:05:55 +0700

    Update query Top5

--- src/main/java/nghiendt/repository/ProductRepository.java
+++ src/main/java/nghiendt/repository/ProductRepository.java
@@ -11,12 +11,14 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     @Modifying
-    @Query(value = "SELECT DISTINCT TOP 5 p.id as id, p.name as product_name, p.price as product_price, p.quantity product_quantity,\n" +
-            "\tp.image product_image, c.name as company_name, u.username as user_name, p.available as product_available\n" +
-            "\tFROM Products as p\n" +
-            "\tINNER JOIN Users u ON u.id = p.user_id\n" +
-            "\tINNER JOIN Companies c ON c.id = p.company_id\n" +
-            "\tORDER BY product_quantity DESC", nativeQuery = true)
+    @Query(value = "SELECT DISTINCT TOP 5 p.id as id, p.name as product_name, p.price as product_price, od.quantity order_quantity,\n" +
+            "\tp.available as product_available, p.image product_image, c.name as company_name, u.fullname as full_name\n" +
+            "\tFROM OrderDetails od\n" +
+            "\t\tJOIN Orders o ON o.id = od.order_id\n" +
+            "\t\tJOIN Users u ON u.id = o.user_id\n" +
+            "\t\tJOIN Products as p ON p.user_id = u.id\n" +
+            "\t\tJOIN Companies c ON c.id = p.company_id\n" +
+            "\tORDER BY order_quantity DESC", nativeQuery = true)
     List<Top5> getTop5SP();
 
     public static interface Top5 {
@@ -27,6 +29,6 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
         String getProduct_available();
         String getProduct_image();
         String getCompany_name();
-        String getUser_name();
+        String getFull_name();
     }
 }
