d1ae11d5d400ac05bd5d820dc9943c3d7d60e30b
duyplus <duyplusdz@gmail.com>
2022-11-09 21:48:24 +0700

    Update main

--- src/main/java/nghiendt/ECommerceNghienDTApplication.java
+++ src/main/java/nghiendt/ECommerceNghienDTApplication.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -13,7 +15,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableScheduling
 @OpenAPIDefinition(info = @Info(title = "NghienDT OpenAPI", version = "2.0", description = "A powerful yet easy-to-use suite of API developer tools."))
 //@SecurityScheme(name = "nghiendt", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
-public class ECommerceNghienDTApplication {
+public class ECommerceNghienDTApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ECommerceNghienDTApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ECommerceNghienDTApplication.class, args);
     }
